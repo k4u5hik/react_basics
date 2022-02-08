f13842bb56093135037d164b24e99cf7a7acea94
Kaushik Chemburkar <attherate@gmail.com>
2022-02-08 19:36:17 +1100

    Wrapping with a div vs React Fragment
    - return single element otherwise there is an error
    - div/section/article tag can be returned
    - use camelCase for HTML attribute
    - className instead of class
    - close/self close every element
    - formatting - make sure that the opening tag of the element is in the same line as return otherwise there will be an error OR you can add parenthesis in the same line instead and wrap the code in it.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom'
 
 function Greeting() {
   return (
-    <div>
-      <h1>Hello World! This is my first component. It is working!</h1>
-    </div>
+    <React.Fragment>
+      <div>
+        <h1>Hello World! This is my first component. It is working!</h1>
+        <ul>
+          <li>
+            <a href='#'>Hello People</a>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
   )
 }
-
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'Hello World!')
-//   )
-// }
-// this code is how it works but is more unreadable than the above code
 
 ReactDOM.render(<Greeting />, document.getElementById('root'))
