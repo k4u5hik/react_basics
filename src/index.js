8b6d036bf07b70539277b96df331cc2def45794d
Kaushik Chemburkar <attherate@gmail.com>
2022-02-08 17:08:04 +1100

    self closing tags required for react statements
    Lesson 17 - Text editor setup
    
    - changed some settings in vscode and installed some extensions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function Greeting() {
-  return <h1>Hello World! This is my first component</h1>;
+  return <h1>Hello World! This is my first component</h1>
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+ReactDOM.render(<Greeting />, document.getElementById('root'))
+
+// changed vscode settings
