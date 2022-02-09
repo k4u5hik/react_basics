17a369204b997a059d763b8febf970164c1dc929
Kaushik Chemburkar <attherate@gmail.com>
2022-02-10 09:59:30 +1100

    alternate way

--- src/index.js
+++ src/index.js
@@ -38,13 +38,14 @@ function BookList() {
   )
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { title, author, img } = props
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1> {title} </h1>
       <h4> {author} </h4>
-      {children}
+      {props.children}
     </article>
   )
 }
