b4073bf7159bb9127d87911c760443f63c80abcf
Kaushik Chemburkar <attherate@gmail.com>
2022-02-09 16:17:47 +1100

    nesting components inside other components

--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ function BookList() {
   return (
     <section>
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 
 const Book = () => {
-  return <article>this is a book</article>
+  return (
+    <article>
+      <Title></Title>
+      <Image></Image>
+      <Author></Author>
+    </article>
+  )
 }
+const Image = () => (
+  <img
+    src='https://m.media-amazon.com/images/I/91uFdkCJmAL._AC_UY436_FMwebp_QL65_.jpg'
+    alt=''
+  />
+)
+
+const Title = () => <h1> This is the book title </h1>
+const Author = () => <h4> This is the book author </h4>
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
