9c769b70b6725ec41f912f864c99b86394608a45
Kaushik Chemburkar <attherate@gmail.com>
2022-02-11 10:40:21 +1100

    onClick event added to title with console log returning title

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const Book = ({ title, author, img }) => {
   return (
     <article className='book'>
       <img src={img} alt='' />
-      <h1> {title} </h1>
+      <h1 onClick={() => console.log(title)}> {title} </h1>
       <h4> {author} </h4>
       <button type='button' onClick={clickHandler}>
         {' '}
