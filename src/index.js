c53cf09c91130c4b902f7077f0d62c2bbf68b118
Kaushik Chemburkar <attherate@gmail.com>
2022-02-10 11:44:06 +1100

    Simple List - Refactoring the code to take information from an array object

--- src/index.js
+++ src/index.js
@@ -3,49 +3,41 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 
-const firstBook = {
-  title: 'The Lord of the Rings',
-  author: 'J.R.R. Tolkien',
-  img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU',
-}
-
-const secondBook = {
-  title: 'The Catcher in the Rye',
-  author: 'J.D. Salinger',
-  img: 'https://static.tvtropes.org/pmwiki/pub/images/selindjer_nad_propastyu_vo_rji.jpg',
-}
+const books = [
+  {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU',
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    img: 'https://static.tvtropes.org/pmwiki/pub/images/selindjer_nad_propastyu_vo_rji.jpg',
+  },
+  {
+    title: 'A song of Ice and Fire',
+    author: 'George R.R. Martin',
+    img: 'https://i.harperapps.com/hcanz/covers/9780006479888/x293.jpg',
+  },
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In nostrum
-          fuga culpa nobis libero! Et corrupti accusantium necessitatibus non
-          iure!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book book={book}></Book>
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { title, author, img, children } = props
+  const { title, author, img } = props.book
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1> {title} </h1>
       <h4> {author} </h4>
-      {children}
     </article>
   )
 }
