95b4323c2bfe4a4682d482deb230ecf66567be83
Kaushik Chemburkar <attherate@gmail.com>
2022-02-11 10:24:52 +1100

    Warning: Each child in a list should have a unique "key" prop.
    - console log error
    - added a unique id to each book

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     title: 'The Lord of the Rings',
     author: 'J.R.R. Tolkien',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhsqsuiuuvY0XY1Sh4kwLjZmUsRtj_qYKA6DcpIETPubqUF335Rs_LES6wNt0yxzMqE0g&usqp=CAU',
   },
   {
+    id: 2,
     title: 'The Catcher in the Rye',
     author: 'J.D. Salinger',
     img: 'https://static.tvtropes.org/pmwiki/pub/images/selindjer_nad_propastyu_vo_rji.jpg',
   },
   {
+    id: 3,
     title: 'A song of Ice and Fire',
     author: 'George R.R. Martin',
     img: 'https://i.harperapps.com/hcanz/covers/9780006479888/x293.jpg',
@@ -25,7 +28,7 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book book={book}></Book>
+        return <Book key={book.id} book={book}></Book>
       })}
     </section>
   )
